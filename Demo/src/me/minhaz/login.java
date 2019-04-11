6828c7db48e845641f95e6e23c699cbe19750c78
minhaz044 <minhauddin044@gmail.com>
2019-04-11 19:13:37 +0600

    Servlet to Servlet,RequestDispatcher,Redirect ,Session,Cookie

--- Demo/src/me/minhaz/login.java
+++ Demo/src/me/minhaz/login.java
@@ -2,18 +2,49 @@ package me.minhaz;
 
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class login extends HttpServlet{
 	
 	public void doPost(HttpServletRequest req,HttpServletResponse res) {
+		/*
 		Integer i=Integer.parseInt(req.getParameter("userName"));
 		Integer j=Integer.parseInt(req.getParameter("password"));
+		*/
+		String userName=req.getParameter("userName");
+		String password=req.getParameter("password");
+		
+		String data1="Hello";
+		String data2="Minhaz";
+		
+		Cookie cookie=new Cookie("data1",data1);
+		res.addCookie(cookie);
+		
+		  cookie=new Cookie("data2",data2); 
+		  res.addCookie(cookie);
+	
 		try{
+			
+			
+			//System.out.println("Is in Post");
+			// RequestDispatcher rd=req.getRequestDispatcher("other");
+			// rd.forward(req, res);
+			HttpSession session=req.getSession();
+			session.setAttribute("userName", userName);
+			session.setAttribute("password", password);
+			
+			res.sendRedirect("other");
+			 
+			
+			/*
 			PrintWriter output=res.getWriter();
 			output.println("<h1>"+(i+j)+"</H1>");
+			*/
 		}catch(Exception e) {
 			System.out.println("Error");
 			System.out.println("Error:  "+e);
@@ -22,8 +53,14 @@ public class login extends HttpServlet{
 	public void doGet(HttpServletRequest req,HttpServletResponse res) {
 
 		try{
+			req.setAttribute("k", 50);
+		
+			RequestDispatcher rd=req.getRequestDispatcher("/other");
+			rd.forward(req, res);
+			/*
 			PrintWriter output=res.getWriter();
 			output.println("<h1> Invalid Request GET</H1>");
+			*/
 		}catch(Exception e) {
 			System.out.println("Error");
 			System.out.println("Error:  "+e);
